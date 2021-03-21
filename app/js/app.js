372d1d4990435717f710bf585393afb99e33b838
jokeejoke <69756862+jokeejoke@users.noreply.github.com>
2021-03-21 13:32:49 +0600

    add footer

--- app/js/app.js
+++ app/js/app.js
@@ -1,27 +1,36 @@
-import $ from 'jquery'; window.jQuery = $; window.$ = $ // import module example (npm i -D jquery)
-import Swiper, {Autoplay, Pagination, Navigation} from 'swiper';
+import $ from 'jquery';
+window.jQuery = $;
+window.$ = $ // import module example (npm i -D jquery)
+import Swiper, {
+	Autoplay,
+	Pagination,
+	Navigation
+} from 'swiper';
+import axios from 'axios';
+
 require('~/app/js/vendor/select2-develop/dist/js/select2.min.js');
 
 
 document.addEventListener('DOMContentLoaded', () => {
 
 	/** JQUERY CODE */
+	
 	$(".tab_item").not(":first").hide();
-	$(".tab-wrapper .tab").click(function() {
+	$(".tab-wrapper .tab").click(function () {
 		$(".tab-wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
 		$(".tab_item").hide().eq($(this).index()).fadeIn()
 	}).eq(0).addClass("active");
 
 	$('.filter-sort').select2({
-    dropdownAutoWidth: true,
+		dropdownAutoWidth: true,
 		minimumResultsForSearch: Infinity
 	});
 
-	$("#file-upload").on("change", function() {
+	$("#file-upload").on("change", function () {
 		$(".custom-file-upload span").html($("#file-upload").val());
 	})
 
-	
+
 	/** END JQUERY CODE */
 
 	Swiper.use([Autoplay, Pagination, Navigation]);
@@ -30,8 +39,8 @@ document.addEventListener('DOMContentLoaded', () => {
 	function hamburgerMenu() {
 		const $burgerBtn = document.querySelector('.hamburger')
 		const $mobileMenu = document.querySelector('.header-bottom')
-		$burgerBtn.addEventListener('click', function() {
-			if(this.classList.contains('hamburger__active')){
+		$burgerBtn.addEventListener('click', function () {
+			if (this.classList.contains('hamburger__active')) {
 				this.classList.remove('hamburger__active')
 				$mobileMenu.classList.remove('active')
 				document.body.style.overflow = ''
@@ -66,7 +75,7 @@ document.addEventListener('DOMContentLoaded', () => {
 			1200: {
 				slidesPerView: 2,
 				spaceBetween: 30,
-				
+
 			},
 			768: {
 				slidesPerView: 2,
@@ -79,6 +88,42 @@ document.addEventListener('DOMContentLoaded', () => {
 	hamburgerMenu()
 
 
-
-
 })
+/**
+var app = new Vue({
+  el: '#catalog',
+  data() {
+		return {
+			artistsList: [],
+			showPerPage: 6,
+			page: 1,
+			filter: "",
+			loader: true
+		}
+	},
+	created() {
+		const _self = this
+		axios.get('https://jsonplaceholder.typicode.com/todos')
+		.then((r) => {
+			_self.artistsList = r.data;
+			_self.loader = false
+		})
+		.catch(e => console.log(e));
+		
+	},
+	methods: {
+		filteredArtists() {
+
+			const start = (this.page - 1) * this.showPerPage;
+			const end = this.page * this.showPerPage;
+
+			return this.artistsList.filter(a => a.title.includes(this.filter)).slice(start,end)
+		}
+	},
+	watch: {
+		page() {
+			console.log(typeof this.page);
+		}
+	}
+})
+ */
